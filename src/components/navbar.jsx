1fa59be909ea0e54b1337781c7a02f115b9bd3b7
Starz0r <starz0r@starz0r.com>
2020-01-27 18:54:47 -0600

    navbar: proper navigation using Links from react-router-dom

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import 'bulmaswatch/cyborg/bulmaswatch.min.css';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar is-danger" role="navigation" aria-label="main navigation">
+            <nav className="navbar is-danger is-active" role="navigation" aria-label="main navigation">
                 <div className="navbar-menu">
                     <div className="navbar-start">
+                        <Link className="navbar-item" to="/">
                             Home
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </nav>
