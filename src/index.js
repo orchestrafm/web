87942a24349062cdae26727556ab7d698168ef82
Starz0r <starz0r@starz0r.com>
2020-02-08 17:57:25 -0600

    index: css helmet for horizontal cards, background color and images

--- src/index.js
+++ src/index.js
@@ -28,6 +28,62 @@ class Main extends Component {
                     </Switch>
                 </div>
             </Router>
+            <ToastProvider ref={window.__react_toast_provider}>
+                <Router>
+                    <div className="App">
+                        <Helmet>
+                            <style>
+                            {
+                                `html, body, #background {
+                                    background-color: #242424;
+                                    background-image: url(./squares.png);
+                                }
+
+                                /* Copied from https://github.com/jgthms/bulma/pull/1596#issuecomment-556416927 */
+                                /* Remove if horizontal cards get merged */
+                                .card.is-horizontal {
+                                    flex-direction: row;
+                                    display: flex;
+                                    flex-basis: 50ex;
+                                    flex-grow: 0;
+                                    flex-shrink: 1;
+                                    box-shadow: none;
+                                }
+
+                                .card.is-horizontal .card-image {
+                                    align-self: center;
+                                }
+
+                                .card.is-horizontal .image {
+                                    min-height: 100%;
+                                }
+
+                                .card.is-horizontal .card-content {
+                                    flex: 1;
+                                }
+
+                                .card.is-horizontal .card-content {
+                                    padding-left: 1em;
+                                    padding-top: 0;
+                                    padding-bottom: 0;
+                                    font-size: 0.8em;
+                                }
+
+                                .card.is-horizontal {
+                                    ul {
+                                        list-style: none;
+                                        margin: 0;
+                                    }
+
+                                    .is-divider {
+                                        margin-top: 1.5rem;
+                                        margin-bottom: 1rem;
+                                    }
+                                }
+                                `
+                            }
+                            </style>
+                        </Helmet>
         )
     }
 }
