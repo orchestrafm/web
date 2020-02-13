4cb8a5ebb4efa988be22b4842a69947cd7e50318
Starz0r <starz0r@starz0r.com>
2020-02-13 14:55:15 -0600

    footer: reduce padding size

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import { Footer as RBXFooter, Container, Hero, Content } from 'react-bulma-components';
+import styled, { css } from 'styled-components'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 class Footer extends Component {
     render() {
+        const SmallerPadding = styled.div`
+        &&&&& {
+            padding-top: 1rem!important;
+            padding-bottom: 1rem!important;
+        }`
         return (
             <div>
             &nbsp;
-                <Hero className="is-medium">
+                <Hero
+                className="is-medium"
+                >
                     <Hero.Footer>
-                        <RBXFooter>
+                        <RBXFooter
+                        renderAs={SmallerPadding}
+                        >
                             <Container>
-                                <Content className="has-text-left" style={{ textAlign: 'left' }}>
-                                    <p className="has-text-left">
-                                        Copyright <i className="fa fa-copyright" alt="©" aria-hidden="true"></i> 2020, Orchestra FM. Source Code is licensed under <a href="https://opensource.org/licenses/AGPL-3.0.php">GNU Affero General Public License 3</a>.
+                                <Content
+                                className="has-text-left" style={{ textAlign: 'left' }}
+                                >
+                                    <p
+                                    className="has-text-left"
+                                    >
+                                        Copyright&nbsp;
+                                        <i
+                                        className="fa fa-copyright"
+                                        alt="©"
+                                        aria-hidden="true"
+                                        />
+                                        &nbsp;2020, Orchestra FM. Source Code is licensed under
+                                        <a
+                                        href="https://opensource.org/licenses/AGPL-3.0.php"
+                                        >
+                                        &nbsp;GNU Affero General Public License 3
+                                        </a>
+                                        .
                                     </p>
                                 </Content>
                             </Container>
