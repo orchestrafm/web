b49d4fb42e15c1e2d659a9dbb14ff541a3416105
Starz0r <starz0r@starz0r.com>
2020-11-06 17:03:05 -0600

    app: patched bulma css for horizontal cards

--- src/App.vue
+++ src/App.vue
@@ -39,4 +39,51 @@ html body #app {
 .content {
     flex: 1;
 }
+
+/*
+Bulma Related CSS Additions Below
+
+GitHub Issue: https://github.com/jgthms/bulma/pull/1596#issuecomment-556416927
+When this gets merged this should be able to be deleted.
+*/
+.card.is-horizontal {
+  flex-direction: row;
+  display: flex;
+  flex-basis: 50ex;
+  flex-grow: 0;
+  flex-shrink: 1;
+  box-shadow: none;
+}
+
+.card.is-horizontal .card-image {
+  align-self: center;
+}
+
+.card.is-horizontal .image {
+  min-height: 100%;
+}
+
+.card.is-horizontal .card-content {
+  flex: 1;
+}
+
+.card.is-horizontal .card-content {
+  padding-left: 1em;
+  padding-top: 0;
+  padding-bottom: 0;
+  font-size: 0.8em;
+}
+
+.card.is-horizontal {
+  ul {
+    list-style: none;
+    margin: 0;
+  }
+
+  .is-divider {
+    margin-top: 1.5rem;
+    margin-bottom: 1rem;
+  }
+}
+/* Bulma Related CSS Additions Ends */
 </style>
