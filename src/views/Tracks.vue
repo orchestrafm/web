ca7104b0c0a840201c437bb59c68a1c5dc386f67
Starz0r <starz0r@starz0r.com>
2020-11-09 07:49:35 -0600

    tracks: display list of trackcards

--- src/views/Tracks.vue
+++ src/views/Tracks.vue
@@ -1,9 +1,29 @@
 <template>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis porta ligula, a auctor purus aliquet vulputate. Nunc eu lacinia nibh. Sed congue porttitor maximus. Sed vehicula odio ut tellus interdum, non pretium tellus lacinia. Suspendisse a tortor gravida, pretium urna a, pretium mauris. Fusce at lacus turpis. Ut molestie arcu lorem, ut maximus nibh suscipit a. Maecenas eleifend arcu id mauris dignissim, nec suscipit nisl commodo. Suspendisse tincidunt semper fringilla. Fusce et dignissim orci. Ut sem felis, fringilla non iaculis et, interdum in mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla nec tempor sem. Morbi aliquet, mauris sed venenatis posuere, quam est hendrerit urna, non tempor nisl tellus eu enim. Nunc eget nunc id turpis tincidunt convallis.
+	<div class="container is-fluid">
+		<div class="columns is-multiline">
+			<div v-for="track in trackList" class="column is-half">
+				<trackcard :key="track.id" v-bind:id="track.id" v-bind:title="track.title" v-bind:artists="track.artists"/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import trackcard from "@/components/trackcard.vue";
+import api from "@/api";
+
 export default {
-    name: "Tracks"
+    name: "Tracks",
+	data() {
+		return {
+			trackList: []
+		};
+	},
+	async beforeCreate() {
+		this.trackList = await api.track.getByRecents(8);
+	},
+	components: {
+        trackcard
+    }
 }
 </script>
