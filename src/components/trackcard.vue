62afab3b901585bf81f8c3b4859393ce5129fa0f
Starz0r <starz0r@starz0r.com>
2020-11-09 07:48:39 -0600

    trackcard: shows information about track and related boards

---
+++ src/components/trackcard.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="card is-horizontal">
+		<div class="card-image is-horizontal">
+			<figure class="image is-160x160 is-horizontal">
+				<img :key="this.boardJacket" v-bind:src="this.boardJacket" alt="Placeholder image" style="width:160px;height:160px;"/>
+			</figure>
+		</div>
+		
+		<div class="card-content is-horizontal">
+	
+			<div class="media-content">
+				<p class="title is-4">{{title}}</p>
+				<p class="subtitle is-6">{{artists}}</p>
+			</div>
+	
+			<div class="content is-horizontal">
+	
+				<div> &nbsp; </div>
+				<div> &nbsp; </div>
+				<div> &nbsp; </div>
+				<div> &nbsp; </div>
+				
+				<div class="container columns">
+					<div v-for="board in boardList" class="column field has-addons">
+						<boardbutton v-bind:rating="board.difficulty_rating" v-bind:diff="board.difficulty_name" v-bind:track_id="board.track_id" v-bind:board_id="board.id"/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import boardbutton from "@/components/boardbutton.vue";
+import api from "@/api";
+import axios from 'axios';
+
+export default {
+    name: "trackcard",
+	props: ["id", "title", "artists"],
+	components: {
+		boardbutton
+    },
+	data() {
+		return {
+			boardList: [],
+			boardJacket: "https://via.placeholder.com/160x160.webp"
+		};
+	},
+	async created() {
+		let boards = await api.board.getFromTrack(this.id);
+		this.boardList = await boards.sort(function (propName) {
+			return function(a, b) {
+				if (a[propName] > b[propName]) {
+					return 1;
+				} else if (a[propName] < b[propName]) {
+					return -1;
+				}
+				return 0;
+			}
+		}("difficulty_rating"));
+		this.boardJacket = this.boardList[0].jacket;
+	}
+}
+</script>
