ad193dfb0c76940a1ca1b3eff4d5eb129cb4f35e
Starz0r <starz0r@starz0r.com>
2020-11-12 04:36:32 -0600

    api: get count of total submitted tracks

--- src/api/track.js
+++ src/api/track.js
@@ -3,14 +3,22 @@ import axios from 'axios';
 const baseurl = "https://orchestra.fm/api/v0"
 
 export default {
-    async getByRecents(amount) {
-        const resp = await axios.get(baseurl + "/track/recent/" + amount);
-		
-		return await resp.data;
-    },
-    async get(id) {
+	async get(id) {
         const resp = await axios.get(baseurl + "/track/" + id);
 		
 		return await resp.data;
-    }
+    },
+	async getCount() {
+		const resp = await axios.get(baseurl + "/track");
+		
+		return await resp.data.count;
+	},
+    async getByRecents(amount, offset) {
+		if (offset == null) {
+			offset = 0;
+		}
+        const resp = await axios.get(baseurl + "/track/recent/" + amount + "?offset="+offset);
+		
+		return await resp.data;
+    },
 }
